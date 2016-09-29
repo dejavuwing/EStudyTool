1aa893bb2f5e0b86a017de264f944e879e335023
dejavuwing <dejavuwing@gmail.com>
2016-09-29 10:11:31 +0900

    youtube finish

--- EStudyTool/Youtube/ChannelsTableViewController.swift
+++ EStudyTool/Youtube/ChannelsTableViewController.swift
@@ -11,23 +11,20 @@ import SwiftyJSON
 
 class ChannelsTableViewController: UITableViewController {
     
-    
     @IBOutlet weak var menuButton: UIBarButtonItem!
     @IBOutlet weak var tblVideos: UITableView!
     
-    var apiKey = "AIzaSyB7axvVjh9cQtbuqpbdBcMibbCcKDPwvPA"
+    let apiKey: String = "AIzaSyB7axvVjh9cQtbuqpbdBcMibbCcKDPwvPA"
     
-    var desiredChannelsArray: [String] = []
+    //var desiredChannelsArray: [String] = []
     var channelIndex = 0
     
-    // var channelsDataArray: Array<Dictionary<String, String>> = []
     var channelsDataArray: [[String: String]] = []
-    var videosArray: Array<Dictionary<NSObject, AnyObject>> = []
-    var selectedVideoIndex: Int!
+    var selectedChannelIndex: Int!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         // 사이드바 메뉴 설정
         if revealViewController() != nil {
@@ -39,27 +36,30 @@ class ChannelsTableViewController: UITableViewController {
         // 로딩 이미지를 노출시킨다.
         ActivityModalView.shared.showActivityIndicator(view: self.view)
         
-        // Youtube 체널 정보를 가져온다.
-        desiredChannelsArray = getChannelListJSON()
+        // ChannelList를 불러온다. (closure의 return 방법 확인)
+        getChannelListJSON() {(response) in
+            if let desiredChannelsArray = response as? [String] {
+                print(desiredChannelsArray)
+                self.getChannelDetails(channells: desiredChannelsArray)
+            }
+        }
         
-        // Youtube 체널 정보를 가져온다.
-        getChannelDetails()
-
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
 
     
-    // ChannelList를 불러온다.
-    func getChannelListJSON() -> [String] {
+    // ChannelList를 불러온다. (closure의 return 방법 확인)
+    func getChannelListJSON(callback: @escaping ([String]) -> ()) {
         var returnValue: [String] = []
-
+        
         let mySession = URLSession.shared
         let versionUrl = "https://raw.githubusercontent.com/dejavuwing/EStudyTool/master/EStudyTool/Youtube/channelList.json"
         let url: NSURL = NSURL(string: versionUrl)!
+        
         
         let networkTask = mySession.dataTask(with: url as URL) { (data, response, error) -> Void in
             if error != nil {
@@ -76,35 +76,27 @@ class ChannelsTableViewController: UITableViewController {
                             
                         }
                     }
-                    
-                } else {
-                    print("Error")
                 }
+                
+                callback(returnValue)
+                //self.desiredChannelsArray = returnValue
             }
+            
         }
         networkTask.resume()
-        
-        return returnValue
     }
     
-
+    
     // Youtube 체널 정보를 가져온다.
-    func getChannelDetails() {
-        
-        // Youtube 체널 정보를 가져온다.
-        if desiredChannelsArray == [] {
-            desiredChannelsArray = ["Apple", "Google", "EnglishLessons4U", "Microsoft"]
-        }
-        
-        //print(desiredChannelsArray)
+    func getChannelDetails(channells: [String]) {
         
         var urlString: String!
         let mySession = URLSession.shared
         
-        urlString = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails,snippet&forUsername=\(desiredChannelsArray[channelIndex])&key=\(apiKey)"
-        let url: NSURL = NSURL(string: urlString)!
+        print("channel : \(channells)")
         
-        print(urlString)
+        urlString = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails,snippet&forUsername=\(channells[channelIndex])&key=\(apiKey)"
+        let url: NSURL = NSURL(string: urlString)!
         
         let networkTask = mySession.dataTask(with: url as URL) { (data, response, error) -> Void in
             if error != nil {
@@ -133,23 +125,16 @@ class ChannelsTableViewController: UITableViewController {
                         
                         // Load the next channel data (if exist).
                         self.channelIndex += 1
-                        if self.channelIndex < self.desiredChannelsArray.count {
-                            self.getChannelDetails()
+                        if self.channelIndex < channells.count {
+                            self.getChannelDetails(channells: channells)
                         }
-                        else {
-                            
-                            //ActivityModalView.shared.hideActivityIndicator()
-                        }
-                        
                     }
-                } else {
-                    print("Error~~~")
                 }
             }
         }
         networkTask.resume()
-        //print("------>\(self.channelsDataArray)")
     }
+
 
     
     
@@ -173,10 +158,7 @@ class ChannelsTableViewController: UITableViewController {
         let thumbnailImageView = cell.viewWithTag(12) as! UIImageView
         
         let channelDetails = channelsDataArray[indexPath.row]
-
-        
-         print(self.channelsDataArray[0]["title"])
-         print(channelDetails["title"])
+        print(channelDetails["title"])
         
         channelTitleLabel.text = channelDetails["title"]
         channelDescriptionLabel.text = channelDetails["description"]
@@ -188,17 +170,29 @@ class ChannelsTableViewController: UITableViewController {
         return cell
     }
     
-
-    
-    
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-            selectedVideoIndex = indexPath.row
-            performSegue(withIdentifier: "idSeguePlayer", sender: self)
+            selectedChannelIndex = indexPath.row
+            performSegue(withIdentifier: "goYoutubeView", sender: self)
         
     }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var selectedChannel: String = ""
+        
+        if (segue.identifier == "goYoutubeView") {
+            
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                
+                let channelDetails = channelsDataArray[indexPath.row]
+                print(channelDetails["id"])
+                
+                selectedChannel = channelDetails["id"]!
+            }
+            
+            let controller = segue.destination as? youtubeWebController
+            controller!.selectedChannel = selectedChannel
+        }
+    }
     
 }
